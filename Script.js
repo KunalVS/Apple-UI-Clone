08fa437b7b89de95dafda3c8857e448fa7935268
Kunal Shirke <shirkekunal91@gmail.com>
2025-09-27 15:51:07 +0530

    Added script file

---
+++ Script.js
@@ -0,0 +1,69 @@
+let btn1=document.getElementById("btn1");
+btn1.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/iphone-17-pro/";
+});
+let btn2=document.getElementById("btn2");
+btn2.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_iphone/iphone_17_pro";
+});
+let btn3=document.getElementById("btn3");
+btn3.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/iphone-air/";
+});
+let btn4=document.getElementById("btn4");
+btn4.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_iphone/iphone_air";
+});
+let btn5=document.getElementById("btn5");
+btn5.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/iphone-17/";
+});
+let btn6=document.getElementById("btn6");
+btn6.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_iphone/iphone_17";
+});
+let btn7=document.getElementById("btn7");
+btn7.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/airpods-pro/";
+});
+let btn8=document.getElementById("btn8");
+btn8.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_airpods/airpods_pro_3";
+});
+let btn9=document.getElementById("btn9");
+btn9.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/apple-watch-series-11/";
+});
+let btn10=document.getElementById("btn10");
+btn10.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_watch/apple_watch_series_11";
+});
+let btn11=document.getElementById("btn11");
+btn11.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/apple-watch-se-3/";
+});
+let btn12=document.getElementById("btn12");
+btn12.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_watch/apple_watch_se";
+});
+let btn13=document.getElementById("btn13");
+btn13.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/apple-watch-ultra-3/";
+});
+let btn14=document.getElementById("btn14");
+btn14.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/buy_watch/apple_watch_ultra_3";
+});
+let btn15=document.getElementById("btn15");
+btn15.addEventListener("click",()=>{
+    window.location.href="#";
+});
+let btn16=document.getElementById("btn16");
+btn16.addEventListener("click",()=>{
+    window.location.href="#";
+});
+let btn17=document.getElementById("btn17");
+btn17.addEventListener("click",()=>{
+    window.location.href="https://www.apple.com/in/shop/goto/trade_in";
+});
+
